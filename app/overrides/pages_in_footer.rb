a7714cebd0efd26e97240abe33955956ffe68fb6
Artur Janicki <janicki.artur@gmail.com>
2013-09-21 23:46:30 +0200

    update footer v3

--- app/overrides/pages_in_footer.rb
+++ app/overrides/pages_in_footer.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "_footer",
+Deface::Override.new(:virtual_path => "spree/shared/_footer",
                      :name => "pages_in_footer",
                      :insert_bottom => "#footer-menu",
                      :partial => "spree/static_content/static_content_footer",
